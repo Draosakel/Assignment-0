6a3c022e774a52c591bc96a62eee86b4e8dfe41a
Christoffer H. Nielsen <hecn@itu.dk>
2022-09-07 20:37:55 +0200

    UserInterface user enter desired year

--- Application/Program.cs
+++ Application/Program.cs
@@ -1,6 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
+leapYear lyear = new leapYear();
+Console.WriteLine("Enter desired year: ");
+int year = Convert.ToInt32(Console.ReadLine());
+if(lyear.IsLeapYear(year)) {
+    Console.WriteLine("Yay");
+} else {
+    Console.WriteLine("Nay");
+}
 public class leapYear {
     public bool IsLeapYear(int year) {
         if(year % 100 == 0 && year % 400 != 0) return false;

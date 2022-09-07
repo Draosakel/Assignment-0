82746bf7361e15db23132f2e5219f6d33696cee0
Christoffer H. Nielsen <hecn@itu.dk>
2022-09-07 18:51:54 +0200

    Tested and implemented IsLeapYear

--- Application/Program.cs
+++ Application/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+public class leapYear {
+    public bool IsLeapYear(int year) {
+        if(year % 100 == 0 && year % 400 != 0) return false;
+        if(year % 4 == 0) return true;
+        return false;
+    }
+}

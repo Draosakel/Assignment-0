9fee6b72ba2459af23172cdba85177a0c933226e
Christoffer H. Nielsen <hecn@itu.dk>
2022-09-07 20:36:33 +0200

    Interface

--- Application/Program.cs
+++ Application/Program.cs
@@ -1,7 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Application;
 
 public class leapYear {
+    static void Main(string[] args) {
+        leapYear lyear = new leapYear();
+        Console.WriteLine("Enter desired year: ");
+        int year = Convert.ToInt32(Console.ReadLine());
+        if(lyear.IsLeapYear(year)) {
+            Console.WriteLine("Yay");
+        } else {
+            Console.WriteLine("Nay");
+        }
+    }
     public bool IsLeapYear(int year) {
         if(year % 100 == 0 && year % 400 != 0) return false;
         if(year % 4 == 0) return true;
